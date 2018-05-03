2832157fedf219bd4bb87c2953480a720fcd8fd0
cdaquieno <38888739+cdaquieno@users.noreply.github.com>
2018-05-03 16:14:01 -0400

    Create sentimentapp.R

---
+++ sentimentapp.R
@@ -0,0 +1,33 @@
+# Load packages
+library(shiny)
+library(dplyr)
+library(syuzhet)
+
+# UI
+ui <- fluidPage(
+  titlePanel("Get sentiment"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput(
+        "text",
+        label = NULL,
+        value = "I love Dr. Soon Chun",
+        width = NULL,
+        placeholder = "Compose text here"
+      )
+    ),
+    mainPanel(
+      tableOutput("table")
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+  output$table <- renderTable({
+    input$text %>% (function(text) tibble(Text = text, Mood = get_sentiment(text)))
+  })
+}
+
+# Configuration
+shinyApp(ui = ui, server = server)
